b4c939e8cb7acda41829e749c8d67ceb9c756c46
SkyAo <csvwolf@qq.com>
2014-09-21 12:27:32 +0800

    优化语句并缩短行数

--- 数据结构/fushu2.c
+++ 数据结构/fushu2.c
@@ -17,76 +17,54 @@ int input(complex *num1) {
         /* 结果储存部分 */
         if (!isdigit(c) && c != '-' && c != '.') {
             digitmode = 0;
-            if (c == 'i' && ipart == 0 && counter == 0 && pcounter == 0) {   // 检测为虚数 虚部未输入
+            if (c == 'i' && !ipart && !counter && !pcounter) {   // 检测为虚数 虚部未输入
                 if (minus) {
                     num1->ipart = -1;
                 } else {
                     num1->ipart = 1;
                 }
                 ipart = 1;
-                point = 0;
-                minus = 0;
-                counter = 0;
-                pcounter = 0;
-                num = 0;
-            } else if (c == 'i' && ipart == 0 && (counter != 0 || pcounter != 0))  {    // 前面检测到数字 虚部未输入
+                point = minus = counter = pcounter = num = 0;
+            } else if (c == 'i' && !ipart && (counter || pcounter))  {    // 前面检测到数字 虚部未输入
                 if (minus) {
                     num1->ipart = -num;
                 } else {
                     num1->ipart = num;
                 }
                 ipart = 1;
-                num = 0;
-                point = 0;
-                minus = 0;
-                pcounter = 0;
-                counter = 0;
-            } else if (c != 'i' && rpart == 0 && (counter != 0 || pcounter != 0)) {    // 检测为实数 实部未输入 已经检测到前面的数据
+                num = point = minus = pcounter = counter = 0;
+            } else if (c != 'i' && !rpart && (counter || pcounter)) {    // 检测为实数 实部未输入 已经检测到前面的数据
                 if (minus) {
                     num1->rpart = -num;
                 } else {
                     num1->rpart = num;
                 }
                 rpart = 1;
-                num = 0;
-                counter = 0;
-                minus = 0;
-                point = 0;
-                pcounter = 0;
-            } else if(rpart == 1 && ipart == 0 && (counter != 0 || pcounter != 0)) {
+                num = counter = minus = point = pcounter = 0;
+            } else if(rpart && !ipart && (counter || pcounter)) {
                 if (minus) {
                     num1->ipart = -num;
                 } else {
                     num1->ipart = num;
                 }
                 ipart = 1;
-                num = 0;
-                counter = 0;
-                minus = 0;
-                point = 0;
-                pcounter = 0;
+                num = counter = minus = point = pcounter = 0;
             }
         /* 读入负号开启负号模式 */
-        } else if (c == '-' && digitmode == 1) {
+        } else if (c == '-' && digitmode) {
             if (minus) {
                 num1->rpart = -num;
             } else {
                 num1->rpart = num;
             }
-            digitmode = 0;
-            minus = 1;
-            rpart = 1;
-            num = 0;
-            counter = 0;
-            point = 0;
-            pcounter = 0;
+            digitmode = num = counter = point = pcounter = 0;
+            minus = rpart = 1;
         } else if (c == '-') {
             digitmode = 0;
             minus = 1;
         /* 读入小数点开始小数点模式 */
         } else if (c == '.') {
-            digitmode = 1;
-            point = 1;
+            digitmode = point = 1;
             pcounter = 0;
         /* 小数点模式数据处理 */
         } else if (isdigit(c) && point) {
@@ -113,13 +91,13 @@ int input(complex *num1) {
     }
     fflush(stdin);
 
-    if (ipart == 0 && rpart == 0) {
+    if (!ipart && !rpart) {
         printf("发生错误，识别不能，重新输入看看\n");
         return 1;
-    } else if (ipart == 0 && rpart == 1) {
+    } else if (!ipart && rpart) {
         num1->ipart = 0;
         return 0;
-    } else if (ipart == 1 && rpart == 0) {
+    } else if (ipart && !rpart) {
         num1->rpart = 0;
         return 0;
     } else {
@@ -162,13 +140,13 @@ void output(complex num3)
         printf("%g+i\n", num3.rpart);
     } else if (num3.ipart == -1 && num3.rpart != 0) {
         printf("%g-i\n", num3.rpart);
-    } else if (num3.ipart == 1 && num3.rpart == 0) {
+    } else if (num3.ipart == 1 && !num3.rpart) {
         printf("i\n");
-    } else if (num3.ipart == -1 && num3.rpart == 0) {
+    } else if (num3.ipart == -1 && !num3.rpart) {
         printf("-i\n");
-    } else if (num3.ipart == 0) {
+    } else if (!num3.ipart) {
         printf("%g\n", num3.rpart); // 虚部为0 不显示
-    } else if (num3.rpart == 0) {
+    } else if (!num3.rpart) {
         printf("%gi\n", num3.ipart);    // 实部为0 不显示
     } else if (num3.ipart > 0) {
         printf("%g+%gi\n", num3.rpart, num3.ipart); // 虚部大于0 手动增加加号
@@ -190,7 +168,7 @@ int main(void)
             printf("输入两个复数咯~a+bi,b+ai都行，手滑打错也行！我会努力看懂的！\n");
             state = 0;
             do {
-                if (state == 1) {
+                if (state) {
                     printf("对不起我会好好加油的！请再次努力的调教我吧\n");
                 }
                 fflush(stdin);
@@ -206,7 +184,7 @@ int main(void)
             state = 0;
             fflush(stdin);
             do {
-                if (state == 1) {
+                if (state) {
                     printf("对不起我会好好加油的！请再次努力的调教我吧\n");
                 }
                 fflush(stdin);
