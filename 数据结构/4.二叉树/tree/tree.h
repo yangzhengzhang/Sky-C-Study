7239f509f8b0388a6f54f527456f7265c0737a30
SkyAo <csvwolf@qq.com>
2014-11-06 23:21:07 +0800

    二叉树 课件版

---
+++ 数据结构/4.二叉树/tree/tree.h
@@ -0,0 +1,17 @@
+#ifndef __TREE_H_
+#define _TREE_H_
+#include <stdbool.h>
+
+typedef struct item
+{
+    char petname[20];
+    char petkind[20];
+} Item;
+
+#define MAXITEMS 10
+typedef struct node
+{
+
+}
+
+#endif // __TREE_H_
---
+++ 数据结构/4.二叉树/tree2.c
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char TElemType;
+
+typedef struct BiTNode
+{
+    TElemType data;
+    struct BiTNode *lchild;
+    struct BiTNode *rchild;
+} BiTNode, *BiTree;
+
+void Show(TElemType elem)
+{
+    printf("%c" , elem);
+}
+
+/* 先序遍历 */
+void Preorder(BiTree T, void(*visit)(TElemType elem))
+{
+    if (T)
+    {
+        printf("%c" , T->data);
+        Preorder(T->lchild, visit);
+        Preorder(T->rchild, visit);
+    }
+}
+
+/* 中序遍历 */
+void Inorder(BiTree T, void(*visit)(TElemType e))
+{
+    if (T)
+    {
+        Preorder(T->lchild, visit);
+        visit(T->data);
+        Preorder(T->rchild, visit);
+    }
+}
+
+
+/* 后序遍历 */
+void Postorder(BiTree T, void(*visit)(TElemType e))
+{
+    if (T)
+    {
+        Preorder(T->lchild, visit);
+        Preorder(T->rchild, visit);
+        visit(T->data);
+    }
+}
+
+/* 统计二叉树中叶子节点的个数 */
+int CountLeaf(BiTree T, int count)
+{
+    if (T)
+    {
+        if((!T->lchild) && (!T->rchild))
+        {
+            count++;
+        }
+        CountLeaf(T->lchild, count);
+        CountLeaf(T->rchild, count);
+    }
+
+    return count;
+}
+
+/* 计算深度 */
+int Depth(BiTree T, int depthval)
+{
+    if (!T)
+    {
+        depthval = 0;
+    }
+    else
+    {
+        depthval = 1 + max(Depth(T->lchild, depthval), Depth(T->rchild, depthval));
+
+    }
+    return depthval;
+}
+
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+/* 新建节点 */
+BiTNode *GetTreeNode(TElemType item, BiTNode *lptr, BiTNode *rptr)
+{
+    BiTNode *T;
+    T = (BiTNode *)malloc(sizeof(BiTNode));
+    T->data = item;
+    T->lchild = lptr;
+    T->rchild = rptr;
+    return T;
+}
+
+/* 复制树 */
+BiTNode *CopyTree(BiTNode *T)
+{
+    BiTNode *newlptr;
+    BiTNode *newrptr;
+    BiTree newT;
+    if (!T)
+    {
+        return NULL;
+    }
+
+    if (T->lchild)
+    {
+        newlptr = CopyTree(T->lchild);
+    }
+    else
+    {
+        newlptr = NULL;
+    }
+
+    if (T->rchild)
+    {
+        newrptr = CopyTree(T->rchild);
+    }
+    else
+    {
+        newrptr = NULL;
+    }
+
+    newT = GetTreeNode(T->data, newlptr, newrptr);
+    return newT;
+}
+
+BiTree CreateBiTree(BiTree T)
+{
+    TElemType elem;
+    scanf("%c", &elem);
+
+    if (elem == ' ')
+    {
+        T = NULL;
+    }
+    else
+    {
+        T = (BiTNode *)malloc(sizeof(BiTNode));
+
+        T->data = elem;
+        T->lchild = CreateBiTree(T->lchild);
+        T->rchild = CreateBiTree(T->rchild);
+    }
+    return T;
+}
+
+
+int main(void)
+{
+    BiTree T;
+    int count = 0;
+    T = CreateBiTree(T);
+    printf("先序：");
+    Preorder(T, Show);
+    printf("\n中序：");
+    Inorder(T, Show);
+    printf("\n后序：");
+    Postorder(T, Show);
+    printf("\n计算深度：");
+    printf("%d ", Depth(T, count));
+    return 0;
+}
